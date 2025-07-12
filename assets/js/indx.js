bc087336caf302e21a102b3c7ce2006dd0e8bd5a
Alina Khametova <alinakhametova1@gmail.com>
2025-07-12 20:54:28 +0300

    add ex1 part1

--- assets/js/indx.js
+++ assets/js/indx.js
@@ -1,1 +1,27 @@
-alert("Hello World");
+//alert("Hello World");
+
+const customer = {
+  firstName: "Adam",
+  lastName: "Walker",
+  userEmail: " Naruto28@gmail.com ",
+  password: "4566783ghs",
+  tel: +7698765432,
+  userAdress: {
+    city: "NY",
+    str: "Riversayd Drayv",
+    h: 6,
+    country: "USA",
+  },
+
+  changeTel(newTel) {
+    this.tel = newTel;
+  },
+};
+console.log("customer", customer);
+
+customer.changeTel(prompt("new tel"));
+console.log(customer);
+
+document.write(
+  ` <p>${customer.userAdress.city}, ${customer.userAdress.str}, ${customer.userAdress.h}, ${customer.userAdress.country}</p>`
+);
